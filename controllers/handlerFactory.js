7a6c55c49a119016416be11ec7b4c4fa93d87a04
mooping66 <yuiesung66@gmail.com>
2021-03-30 16:25:45 +0700

    Update handlerFactory

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -4,15 +4,16 @@ const APIFeatures = require('./../utils/apiFeatures');
 
 //@@Building Handler Factory Functions: Delete
 //@@ Handling DELETE Requests
-catchAsync(async (req, res, next) => {
-  const doc = await Model.findByIdAndDelete(req.params.id);
+exports.deleteOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndDelete(req.params.id);
 
-  if (!doc) {
-    return next(new AppError('No document found with that ID', 404));
-  }
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
 
-  res.status(204).json({ status: 'success', data: null });
-});
+    res.status(204).json({ status: 'success', data: null });
+  });
 
 //@@ Factory Functions: Update and Create
 //@@ Handling PATCH Requests
